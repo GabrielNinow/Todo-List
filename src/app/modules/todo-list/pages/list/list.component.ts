c0cc6e86c8c14686eaab31cd3ce9bfe2550d5ace
gabriel ninow <gabrielninow@gmail.com>
2024-01-29 11:09:34 -0300

    post-deploy: finished v1

--- src/app/modules/todo-list/pages/list/list.component.ts
+++ src/app/modules/todo-list/pages/list/list.component.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
 import { IListItems } from '../../interface/IListItems.interface';
+import { InputListItemComponent } from '../../components/input-list-item/input-list-item.component';
+import { ElocalStorage } from '../../enum/ElocalStorage.enum';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [InputAddItemComponent],
+  imports: [InputAddItemComponent, InputListItemComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
@@ -16,17 +19,102 @@ export class ListComponent {
   public getListItems = this.#setListItems.asReadonly();
 
   #parseItems() {
-    return JSON.parse(localStorage.getItem('@my-list') || '[]');
+    return JSON.parse(localStorage.getItem(ElocalStorage.MY_LIST) || '[]');
   }
+
+  #updateLocalStorage() {
+    return localStorage.setItem(
+      ElocalStorage.MY_LIST, JSON.stringify(this.#setListItems())
+    );
+  }
+
   public getInputAndAddItem(value: IListItems) {
-    //console.log(value);
-    localStorage.setItem('@my-list', JSON.stringify([...this.#setListItems(), value]));
+    localStorage.setItem(ElocalStorage.MY_LIST, JSON.stringify([...this.#setListItems(), value]));
 
     return this.#setListItems.set(this.#parseItems());
+  }
+
+  public listItemsStage(value: 'todo' | 'finished') {
+    return this.getListItems().filter((res: IListItems) => {
+      if (value === 'todo') {
+        return !res.checked;
+      }
+
+      if (value === 'finished') {
+        return res.checked;
+      }
+
+      return res;
+    });
+  }
+
+  public updateItemCheckbox(newItem: { id: string; checked: boolean }) {
+    this.#setListItems.update((oldValue: IListItems[]) => {
+      oldValue.filter((res) => {
+        if (res.id === newItem.id) {
+          res.checked = newItem.checked;
+          return res;
+        }
+
+        return res;
+      });
+
+      return oldValue;
+    });
+
+    return this.#updateLocalStorage();
+  }
+
+  public updateItemText(newItem: { id: string, value: string }) {
+    this.#setListItems.update((oldValue: IListItems[]) => {
+      oldValue.filter((res) => {
+        if (res.id === newItem.id) {
+          res.value = newItem.value;
+          return res;
+        }
+
+        return res;
+      });
+
+      return oldValue;
+    });
+
+    return this.#updateLocalStorage();
+  }
+
+  public deleteItem(id: string) {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete this task!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.#setListItems.update((oldValue: IListItems[]) => {
+          return oldValue.filter((res) => res.id !== id);
+        });
+
+        return this.#updateLocalStorage();
+      }
+    });
+
   }
 
   public deleteAllItems() {
-    localStorage.removeItem('@my-list');
-    return this.#setListItems.set(this.#parseItems());
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete all tasks!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem(ElocalStorage.MY_LIST);
+        return this.#setListItems.set(this.#parseItems());
+      }
+    });
+
+
   }
 }
